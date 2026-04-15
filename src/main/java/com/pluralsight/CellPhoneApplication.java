eeb0a9124681dedbcdbff01556e4845b01b3c1b8
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-15 15:28:46 -0400

    exercise one updates

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,14 +1,30 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
-        CellPhone iphone = new CellPhone(321,"IphonePro","Verizon","(678)-322-1234","Lilith Macelhanon");
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println(iphone.getSerialNumber());
-        System.out.println(iphone.getModel());
-        System.out.println(iphone.getCarrier());
-        System.out.println(iphone.getPhoneNumber());
-        System.out.println(iphone.getOwner());
+        CellPhone cellphone = new CellPhone();
+
+        System.out.print("What is the serial number? ");
+        cellphone.setSerialNumber(scan.nextInt());
+        scan.nextLine();
+
+        System.out.print("What model is the phone? ");
+        String model = scan.nextLine();
+
+        System.out.print("What carrier is the phone? ");
+        String carrier = scan.nextLine();
+
+        System.out.print("Whats your phone number? ");
+        String phoneNumber = scan.nextLine();
+
+        System.out.print("Whats your name? ");
+        String owner = scan.nextLine();
+
+
 
     }
 }
