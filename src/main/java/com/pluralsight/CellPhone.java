f31c2936d1b1b4344ba605b783b11cde63f958f1
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-15 13:16:02 -0400

    exercise one updates

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,37 @@
 package com.pluralsight;
 
 public class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+
+
+    }
+    public int getSerialNumber() {
+        return this.serialNumber;
+    }
+    public String getModel() {
+        return this.model;
+    }
+    public String getCarrier() {
+        return this.carrier;
+    }
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+    public String getOwner() {
+        return this.owner;
+    }
+
+
+
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        CellPhone iphone = new CellPhone(321,"IphonePro","Versizon","(678)-322-1234","Lilith Macelhanon");
+
+        System.out.println(iphone.getSerialNumber());
+        System.out.println(iphone.getModel());
+        System.out.println(iphone.getCarrier());
+        System.out.println(iphone.getPhoneNumber());
+        System.out.println(iphone.getOwner());
+
+    }
 }
